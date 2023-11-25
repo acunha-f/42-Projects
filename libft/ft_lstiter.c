3e06aaaeedfa7bc0572cd19f4d829af62d21f055
André Faria <andremc.faria2001@gmail.com>
2023-11-25 05:31:10 +0000

    almost there-h

---
+++ libft/ft_lstiter.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstiter.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/25 05:06:21 by marvin            #+#    #+#             */
+/*   Updated: 2023/11/25 05:06:21 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	t_list	*temp;
+
+	temp = lst;
+	f(temp->content);
+	while (temp->next != NULL)
+	{
+		f(temp->content);
+		temp = temp->next;
+	}
+}
